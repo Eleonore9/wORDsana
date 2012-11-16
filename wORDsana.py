14531eefaf23eff121f7acd206e18a0229a20851
Eleonore9 <eleonore.mayola@ymail.com>
2012-11-16 15:56:52 -0800

    trying to go on though the recorder doesn't work yet

--- wORDsana.py
+++ wORDsana.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 # -*- coding=UTF-8 -*-
 import os, sys
-from flask import Flask, render_template, redirect, request, url_for, session, g
+from flask import Flask, render_template, redirect, request, url_for, session
 
 
 
@@ -9,20 +9,26 @@ app = Flask(__name__)
 
 
 #---------------To login and access the app--------------------------
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("login.html")
 
-# @app.route("/sign_up")
-# def sign_up():
-#     return render_template("sign_up.html")
+@app.route("/sign_up")
+def sign_up():
+    return render_template("signup.html")
 
+@app.route("/authenticate")
+def authenticate():
+	pass
+
+@app.route("/new_user")
+def new_user():
+	pass
 
 #-----------------------User Page------------------------------------
-# @app.route("/user_page")
-@app.route("/")
+@app.route("/user_page")
 def view_userpage():
-    return render_template("index.html")
+    return render_template("test.html")
 
 
 
